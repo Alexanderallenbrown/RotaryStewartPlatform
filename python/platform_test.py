8a1d0a0f58cf83a3202164c7faa3b65fead24fae
Alex <brownaa@lafayette.edu>
2016-03-16 14:57:43 -0400

    added simple python driver.

--- python/platform_test.py
+++ python/platform_test.py
@@ -20,10 +20,11 @@ ser.close()
 ser.open()
 while ser.isOpen():
 	timenow = time.time()-starttime
-	command = [0,0,int(10*sin(timenow)),0,0,0]
+	command = [int(10*(sin(timenow/3.)+1)),int(10*(sin(timenow/3.)+1)),int(10*(sin(timenow/3.)+1)),int(10*(sin(timenow/3.)+1)),int(10*(sin(timenow/3.)+1)),int(10*(sin(timenow/3.)+1))]
 	ser.write('6')
-	for ind in range(0,len(command)):
-		ser.write(bytes(command[ind]))
+	ser.write(bytearray(command))
+	# for ind in range(0,len(command)):
+	# 	ser.write(bytes(command[ind]))
 	print timenow,command
-	time.sleep(0.1)
+	time.sleep(.1)
 
